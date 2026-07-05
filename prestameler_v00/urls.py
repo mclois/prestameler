13907b285a1931371d33ef83abbe1719af288d6b
Mercedes Cotelo Lois <mclois@gmail.com>
2026-07-05 18:58:21 +0200

    refactor: Rename `prestameler_v00` to `prestameler`

--- prestameler_v00/urls.py
+++ prestameler_v00/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for prestameler project.
+URL configuration for prestameler_v00 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
